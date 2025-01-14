48d29987413fcc2eecfe20cce47b794e3576b17b
Антон <aleksimmortal@mail.ru>
2025-01-14 10:26:33 +0300

    Добавьте файлы проекта.

---
+++ CustomList.cs
@@ -0,0 +1,143 @@
+﻿namespace MyCustomList;
+
+public class MyList
+{
+    private int[] _array;
+    public int Capacity { get; set; }
+    public int Count { get; set; }
+
+    public MyList()
+    {
+        _array = new int[10];
+        Capacity = _array.Length;
+        Count = 0;
+    }
+
+    public MyList(int length)
+    {
+        _array = new int[length];
+        Capacity = length;
+        Count = 0;
+    }
+
+    public MyList(int length, int element)
+    {
+        _array = new int[length];
+        _array[0] = element;
+        Capacity = length;
+        Count = 1;
+    }
+
+    public void Veiwer()
+    {
+        for (int i = 0; i < _array.Length; i++)
+        {
+            Console.WriteLine(_array[i]);
+        }
+    }
+
+    public void Add(int element)
+    {
+        if (Count >= Capacity)
+        {
+            Resize();
+        }
+        _array[Count] = element;
+        Count++;
+    }
+
+    /*Реализовать перегрузку 3 следующих методов Add();23432423432*/
+
+    public void Add(int[] elements)
+    {
+        if ((Count + elements.Length) >= Capacity)
+        {
+            ResizeMas(elements);
+        }
+
+        for (int i = 0; i < elements.Length; i++)
+        {
+            _array[Count++] = elements[i];
+        }
+        Capacity = _array.Length;
+    }
+
+    public void Add(int index, int element)
+    {
+        if (Count >= Capacity)
+        {
+            Resize();
+        }
+        int newLength = _array.Length;
+        var newArray = new int[newLength];
+        Array.Copy(_array, newArray, index);
+        newArray[index] = element;
+        Array.Copy(_array, index, newArray, index + 1, _array.Length - (index + 1));
+        _array = newArray;
+        Count++;
+        Capacity = _array.Length;
+    }
+
+    public void Add(int index, int[] elements)
+    {
+        if ((Count + elements.Length) >= Capacity)
+        {
+            ResizeMas(elements);
+        }
+        int newLength = _array.Length;
+        var newArray = new int[newLength];
+        for (int i = 0; i < index; i++)
+        {
+            newArray[i] = _array[i];
+        }
+        for (int j = 0, k = index; j < elements.Length; j++, k++)
+        {
+            newArray[k] = elements[j];
+        }
+
+        for (int d = index, f = index + elements.Length; d < Count; d++, f++)
+        {
+            newArray[f] = _array[d];
+        }
+        _array = newArray;
+        Count = Count + elements.Length;
+        Capacity = _array.Length;
+    }
+
+    public int this[int index]
+    {
+        get
+        {
+            if (index >= Count || index < 0)
+            {
+                throw new IndexOutOfRangeException();
+            }
+            return _array[index];
+        }
+        set
+        {
+            if (index >= Count || index < 0)
+            {
+                throw new IndexOutOfRangeException();
+            }
+            _array[index] = value;
+        }
+    }
+
+    private void Resize()
+    {
+        int newLength = _array.Length * 2;
+        var newArray = new int[newLength];
+        Array.Copy(_array, newArray, Count);
+        _array = newArray;
+    }
+
+    private void ResizeMas(int[] elementsMas)
+    {
+        int newLength = (_array.Length + elementsMas.Length) * 2;
+        var newArray = new int[newLength];
+        Array.Copy(_array, newArray, Count);
+        _array = newArray;
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿namespace MyCustomList;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        MyList List = new MyList(4);
+        List.Add(14);
+        /*List.Veiwer();
+        List.Add(14);
+        Console.WriteLine();
+        List.Veiwer();
+        Console.WriteLine();
+        List.Add(17);
+        Console.WriteLine();
+        List.Add(28);
+        List.Add(32);*/
+        int[] mass = { 1, 7, 115 };
+        List.Veiwer();
+        Console.WriteLine();
+        Console.WriteLine(List.Count);
+        Console.WriteLine(List.Capacity);
+        Console.WriteLine();
+        List.Add(mass); /* Метод Add(int[] elements)34324324234*/
+        List.Veiwer();
+        Console.WriteLine();
+        Console.WriteLine(List.Count);
+        Console.WriteLine(List.Capacity);
+        Console.WriteLine();
+
+        List.Add(3, 55); /* Метод Add(int index, int element)*/
+        List.Veiwer();
+        Console.WriteLine();
+        Console.WriteLine(List.Count);
+        Console.WriteLine(List.Capacity);
+        Console.WriteLine();
+
+        int[] mass2 = { 96, 248, 715 };
+        List.Add(4, mass2);  /* Метод Add(int index, int[] elements)*/
+        List.Veiwer();
+        Console.WriteLine();
+        Console.WriteLine(List.Count);
+        Console.WriteLine(List.Capacity);
+        Console.ReadKey();
+        Console.ReadKey();
+    }
+}
+
+
